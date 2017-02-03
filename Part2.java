571bf33e524593c1b4e09419af1da2f86a0ca80d
manudee <manudee@gmail.com>
2017-02-02 19:57:59 -0500

    Update Part2.java

--- Part2.java
+++ Part2.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of Part1 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Manasi Sukthankar) 
+ * @version (02/02/2017)
  */
 
 
